a47ba6b9cd5fd2f67b1394042158750da65f68ca
grouchaud <rouchaud.guillaume@gmail.com>
2016-09-21 11:45:08 +0200

    style_subscribe3

--- index.php
+++ index.php
@@ -82,6 +82,10 @@
 				<div class="col-sm-6">
 					<input type="text" name="location" id="INPUT_1" placeholder="Votre email" />
 				</div>
+				<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+				</div>
 				<div class="col-sm-6">
 					<input type="submit" name="commit" value="Essayez maintenant" id="INPUT_2" />
 				</div>
@@ -102,24 +106,23 @@
 </div>
 </div>
 
-
-// <div id="mc_embed_signup">
-// <form action="//zenbase.us13.list-manage.com/subscribe/post?u=bac0e57d55190fa81f4d912f1&amp;id=bdca4a6b9e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    // <div id="mc_embed_signup_scroll">
-	
-// <div class="mc-field-group">
-	// <label for="mce-EMAIL">Email Address </label>
-	// <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-// </div>
-	// <div id="mce-responses" class="clear">
-		// <div class="response" id="mce-error-response" style="display:none"></div>
-		// <div class="response" id="mce-success-response" style="display:none"></div>
-	// </div>
-    // <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_bac0e57d55190fa81f4d912f1_bdca4a6b9e" tabindex="-1" value=""></div>
-    // <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    // </div>
-// </form>
-// </div>
+<!-->
+<div id="mc_embed_signup">
+	<form action="//zenbase.us13.list-manage.com/subscribe/post?u=bac0e57d55190fa81f4d912f1&amp;id=bdca4a6b9e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		<div id="mc_embed_signup_scroll">
+			<div class="mc-field-group">
+				<label for="mce-EMAIL">Email Address </label>
+				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_bac0e57d55190fa81f4d912f1_bdca4a6b9e" tabindex="-1" value=""></div>
+			<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+		</div>
+	</form>
+</div><!-->
 
 
 <section class="section1">
