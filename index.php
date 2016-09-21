5ce53788f66851d24a48954969ad108b7dfdb597
grouchaud <rouchaud.guillaume@gmail.com>
2016-09-21 11:38:11 +0200

    style_subscribe

--- index.php
+++ index.php
@@ -76,15 +76,17 @@
 		</div>
     </div>
 	<div class="hidden-xs col-sm-12">
-		<div class="cta text-center">
-			<p><strong>Commencez votre mois d'essais gratuit dès aujourd'hui !</strong></p>
-			<div class="col-sm-6">
-				<input type="text" name="location" id="INPUT_1" placeholder="Votre email" />
+		<form action="//zenbase.us13.list-manage.com/subscribe/post?u=bac0e57d55190fa81f4d912f1&amp;id=bdca4a6b9e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<div class="cta text-center">
+				<p><strong>Commencez votre mois d'essais gratuit dès aujourd'hui !</strong></p>
+				<div class="col-sm-6">
+					<input type="text" name="location" id="INPUT_1" placeholder="Votre email" />
+				</div>
+				<div class="col-sm-6">
+					<input type="submit" name="commit" value="Essayez maintenant" id="INPUT_2" />
+				</div>
 			</div>
-			<div class="col-sm-6">
-				<input type="submit" name="commit" value="Essayez maintenant" id="INPUT_2" />
-			</div>
-		</div>
+		</form>
 	</div>
 	<div class="hidden-md hidden-sm hidden-lg col-xs-12">
 		<div class="ctaxs text-center">
@@ -100,29 +102,13 @@
 </div>
 </div>
 <!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
 <div id="mc_embed_signup">
 <form action="//zenbase.us13.list-manage.com/subscribe/post?u=bac0e57d55190fa81f4d912f1&amp;id=bdca4a6b9e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-	<h2>Subscribe to our mailing list</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+	
 <div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
+	<label for="mce-EMAIL">Email Address </label>
 	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">First Name </label>
-	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-</div>
-<div class="mc-field-group">
-	<label for="mce-LNAME">Last Name </label>
-	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
 </div>
 	<div id="mce-responses" class="clear">
 		<div class="response" id="mce-error-response" style="display:none"></div>
@@ -133,7 +119,7 @@
     </div>
 </form>
 </div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
 <!--End mc_embed_signup-->
 <section class="section1">
 	<div class="container">
