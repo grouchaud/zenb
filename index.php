20ba19f0a6f603182595eb65e475b69132316bc2
grouchaud <rouchaud.guillaume@gmail.com>
2016-09-21 11:24:38 +0200

    subscribe_mail

--- index.php
+++ index.php
@@ -99,6 +99,26 @@
 	</div>
 </div>
 </div>
+<!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="//zenbase.us13.list-manage.com/subscribe/post?u=bac0e57d55190fa81f4d912f1&amp;id=bdca4a6b9e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address </label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_bac0e57d55190fa81f4d912f1_bdca4a6b9e" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
 <section class="section1">
 	<div class="container">
 		<div class="hidden-sm hidden-lg hidden-md row">
