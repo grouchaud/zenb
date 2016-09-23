f10874b03fea4d368a15e3c3016c8f3382e36c86
grouchaud <rouchaud.guillaume@gmail.com>
2016-09-23 15:43:13 +0200

    fix analytics

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="footer.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
-    <link href="https://emojicss.com/emoji.css" rel="stylesheet">
+<link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.transitions.css">
@@ -22,13 +22,13 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
-	<script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-78726032-1', 'auto');
+  ga('create', 'UA-84644952-1', 'auto');
   ga('send', 'pageview');
 
 </script>
@@ -210,7 +210,7 @@
 					</div>
 					<div class="testimonial">
 						<p class="testimonial-description">
-                        Nous sommes rapidement passés de 10 à 35 collaborateurs, avec des bureaux à Grenoble et à SanFrancisco, et je n’ai plus pu m’entretenir individuellement avec les membres de mon équipe. Grâce à Zenbase, j’ai pu maintenir un contact privilégié, et je sais aujourd’hui comment va chacun de mes collaborateurs.
+                        Nous sommes rapidement passés de 5 à 25 collaborateurs, avec des bureaux à Grenoble et à SanFrancisco, et je n’ai plus pu m’entretenir individuellement avec les membres de mon équipe. Grâce à Zenbase, j’ai pu maintenir un contact privilégié, et je sais aujourd’hui comment va chacun de mes collaborateurs.
 						</p>
 						<div class="testimonial-review">
 							<div class="testimonial-pic">
